9e1de51038a7807be5b9ad4690fba36767943383
ajulissa <57933379+ajulissa@users.noreply.github.com>
2021-12-13 22:43:50 -0500

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def nlp_task(q: NLPProto):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
